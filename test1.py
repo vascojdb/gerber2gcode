e62f80fdd0f0f32f8ed27f27d9f89c9b9fba96dd
Magda <magdalena.wiktoria.szczypka@gmail.com>
2018-11-16 23:06:08 +0100

    Added code to unzip the file and read line by line

--- test1.py
+++ test1.py
@@ -1,2 +1,27 @@
-print ("hello world")
-print ("hello from another world")
+import zipfile
+
+zip_path = "sample_gerber.ZIP"
+extract_path = "bananas"
+
+print("Reading file: " + zip_path)
+
+# Open zip file
+zip_ref = zipfile.ZipFile(zip_path, 'r')
+# Extract files:
+zip_ref.extractall(extract_path)
+# Finally, close the file (mandatory)
+zip_ref.close()
+
+# read the file
+import glob, os
+os.chdir(extract_path)
+for top_paste_filename in glob.glob("*Top SMT Paste Mask*"):
+    print(top_paste_filename)
+
+    with open(top_paste_filename) as f:
+        whole_text = f.read().splitlines()
+
+    print(whole_text)
+
+    for line_number in range(len(whole_text)):
+        print(whole_text[line_number])
